3d95117236b1251ea65c11b0200fe3c3b0347d95
Cameron Bielstein <CTBielstein@gmail.com>
2020-11-04 00:19:34 -0800

    Add some TestServer unit tests (#2)
    Add some TestServer unit tests

---
+++ test/HelloTests/HelloTests.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace HelloTests
+{
+    public class HelloTests
+    {
+        private readonly TestServer server;
+        private readonly HttpClient client;
+
+        public HelloTests()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Hello.Startup>();
+            server = new TestServer(builder);
+            client = server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("Cameron")]
+        [InlineData("Fraveshi")]
+        public async Task TestNameQueryString(string name)
+        {
+            var response = await client.GetAsync($"/hello?name={name}");
+            var message = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal($"Hello, {name}", message);
+        }
+
+        [Fact]
+        public async Task TestNoName()
+        {
+            var response = await client.GetAsync("/hello");
+            var message = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal($"Hello, World", message);
+        }
+    }
+}
