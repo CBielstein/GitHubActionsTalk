3c15a9b9e3a6c6ae1ecf9dc45e586991c5c0112d
Cameron Bielstein <ctbielstein@gmail.com>
2020-11-03 22:06:42 -0800

    Move default weather forecast example to hello world example.

---
+++ src/Hello/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get([FromQuery] string name)
+        {
+            return "Hello, " + (name ?? "World");
+        }
+    }
+}
