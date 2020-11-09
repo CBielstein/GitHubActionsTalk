cbba5bf518fab6927f44be06e88a49550de7ba49
Cameron Bielstein <ctbielstein@gmail.com>
2020-11-08 16:03:21 -0800

    Deploying and service is visible locally.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,12 @@
+dotnet publish --configuration Release --runtime centos.8-x64 --no-self-contained -o published src/Hello/Hello.csproj
+
 # Stop current service
-ssh $USER_HOST systemctl disable helloapp.service
+ssh $USER_HOST systemctl stop helloapp
 
 # rsync to copy files
-rsync -az --delete -e ssh src/Hello/bin/Release/netcoreapp3.1/centos.8-x64/publish/ root@45.33.53.88:/web/hello
+rsync -az --delete -e ssh published/ $USER_HOST:/web/hello
 
 # Resume service
-ssh $USER_HOST systemctl enable helloapp.service
+ssh $USER_HOST systemctl daemon-reload
+ssh $USER_HOST systemctl enable helloapp
+ssh $USER_HOST systemctl start helloapp
